a4ded7c4375b5c3fc3317114516c2d631f8f0644
Javier Uncos <javiersrfc30@gmail.com>
2022-09-23 20:22:52 -0300

    agremos las imagenes a las cards

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Container } from 'react-bootstrap';
 import Perfil from './components/Perfil';
 
 
+
+
 function App() {
   return (
     <div className="App">
-      <Container className="bg-black mt-5">
-        <Perfil />
+      <Container className=" mt-5">
+        <Perfil/>
         <Perfil />
       </Container>
     </div>
--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import '../estilos/Perfil.css'
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import avataresHombre from "../img/avataresHombre.png"
 
 
 const Perfil = () => {
     return (
         <>
         <div className='contenedorPerfil'>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://lh3.googleusercontent.com/-Ek8OBk_hdfc/Wpak83xz7fI/AAAAAAAAOts/uydkIuQNjG8XnNhzP3_BI8440ivghagdwCHMYCw/avatares-perfil-generico%255B3%255D?imgmax=800" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card className="mt-5">
+       <Card.Body className="d-flex ">
+       <Card.Img variant="top" src={avataresHombre} className="imgCardPerfil"/>
+        <div className="mt-5">
+        <Card.Title></Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <span>hola</span>
+        </div>
         </Card.Body>
        </Card>
         </div> 
