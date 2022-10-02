30e82fc375ada7ec92cc817df63e3b2de0be2e7b
Javier Uncos <javiersrfc30@gmail.com>
2022-10-02 17:09:06 -0300

    agregmos avatares de empleados

--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -7,13 +7,18 @@ const EmpleadoList = () => {
 
 
     let arregloEmpleados=[
-        { id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business", pic: "mujer.png" },
-        { id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", pic: "mujer.png" },
-        { id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", pic: "mujer.png" },
-        { id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", pic: "hombre.png" },
-        { id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "hombre.png" },
-        { id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "mujer.png" },
-    ]
+        { id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business", pic: "mujer9.png" },
+        { id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", pic: "mujer2.png" },
+        { id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", pic: "mujer3.png" },
+        { id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", pic: "hombre2.png" },
+        { id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "hombre3.png" },
+        { id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "mujer4.png" },
+        { id: 7, fullName: "Ellen Balderas", title: "Digital Strategist", department: "Marketing", pic: "mujer5.png" },
+        { id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department: "Engineering", pic: "mujer8.png" },
+        { id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department: "Engineering", pic: "hombre4.png" },
+      ]
+      
+  
   
     return (
         <div className="contenedorList">
